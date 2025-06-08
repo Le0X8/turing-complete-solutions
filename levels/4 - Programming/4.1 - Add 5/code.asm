142a6c97fbf95359fcbccd28e94161cd5ef91f62
Le0X8 <84378319+Le0X8@users.noreply.github.com>
2025-06-08 16:12:06 +0200

    addded first 2 code solutions

---
+++ levels/4 - Programming/4.1 - Add 5/code.asm
@@ -0,0 +1,5 @@
+177 # copy from input to reg1
+5   # load value "5" to reg0
+130 # copy from reg0 to reg2
+68  # reg1 + reg2 to reg3
+158 # copy from reg3 to output
---
+++ levels/4 - Programming/4.2 - Calibrating Laser Cannons/code.asm
@@ -0,0 +1,12 @@
+177 # copy input to reg1
+138 # copy reg1 to reg2
+68 # reg1 + reg2 to reg3
+
+153 # copy reg3 to reg1
+154 # copy reg3 to reg2
+68 # reg1 + reg2 to reg3
+
+153 # copy reg3 to reg1
+68 # reg1 + reg3 to reg2
+
+158 # reg3 to output
